a7196a0bc4eb673ddb63122328363d853aa8eea7
anorneto <anor@poli.ufrj.br>
2015-08-31 14:01:23 -0300

    Added lcd functions,will show hour and song playing

--- arduino-usb-media-controller.ino
+++ arduino-usb-media-controller.ino
@@ -1,8 +1,9 @@
 // Anor Batista Esteves Neto 
 // August 2015
-// A simples usb media controller using a pro micro , buttons and a rottary encoder
+// A simples usb media controller using a pro micro , buttons , a rottary encoder and a 5110 lcd to displat information
 // Uses the rotary encoder library(https://github.com/mathertel/RotaryEncoder)  
-// and the Arduino HID project bootloader ( https://github.com/NicoHood/HID )
+// the Arduino HID project bootloader ( https://github.com/NicoHood/HID )
+// and the sparkfun nokia 5110 example (https://github.com/sparkfun/GraphicLCD_Nokia_5110)
 //if the pro micro ever stop being recognized by windows just press upload and quickly connect it to the coomputer  to unbrick it
 #include <RotaryEncoder.h>
 //Rottary encoder connections
@@ -16,26 +17,42 @@ RotaryEncoder encoder(A2, A3);
 #define NEXT_PIN     15 // button connected to ground and to digital pin 15
 #define PREVIOUS_PIN 16 // button connected to ground and to digital pin 16
 
-#define GREEN_LED 8
-#define RED_LED   9
+// mute/unmute status leds
+#define GREEN_LED 8 // on when unmute
+#define RED_LED   9 // on when mute
+//define the nokia lcd pins
+#define LCD_RST   2
+#define LCD_CE    3
+#define LCD_DC    4
+#define LCD_DIN   5
+#define LCD_CLK   6
+
 
 void setup()
 {
-  pinMode(GREEN_LED,OUTPUT);
-  pinMode(RED_LED,OUTPUT);
+  pinMode(GREEN_LED,OUTPUT);// set the green led pin as an output
+  pinMode(RED_LED,OUTPUT); // set the red led pin as an output
   pinMode(SWITCH_PIN, INPUT); // set the switch button port as an input
   pinMode(PLAY_PIN, INPUT); // set the play/pause button port as an input
   pinMode(NEXT_PIN, INPUT); // set the next button port as an input
   pinMode(PREVIOUS_PIN, INPUT); // set the next button port as an input
+
+  LcdInit(); //initializes the lcd
+  LcdClear(); // clears the display, just in case
+  
   digitalWrite(SWITCH_PIN, HIGH); // activate internal pull-up on switch_pin
   digitalWrite(PLAY_PIN, HIGH); // activate internal pull-up on play_pin
   digitalWrite(NEXT_PIN, HIGH); // activate internal pull-up on next_pin
   digitalWrite(PREVIOUS_PIN, HIGH); // activate internal pull-up on next_pin
 
-  digitalWrite(GREEN_LED,HIGH);
-  
-  Consumer.begin();
+  digitalWrite(GREEN_LED,HIGH); // turn unmute led on
+
+  Consumer.begin(); // starts the usb hid control
+
+  LcdString("Hello World!");
 }
+
+
 
 void loop()
 {
@@ -45,27 +62,27 @@ void loop()
   {
     Consumer.write(MEDIA_VOLUME_MUTE);
     
-    digitalWrite(RED_LED, !muted_state_controll);
-    digitalWrite(GREEN_LED, muted_state_controll); 
-    muted_state_controll = !muted_state_controll ;
+    digitalWrite(RED_LED, !muted_state_controll); // when the button is pressed it sends a mute
+    digitalWrite(GREEN_LED, muted_state_controll); // command to the pc .turns off the green led
+    muted_state_controll = !muted_state_controll ; // and on the red led
        
-    while (digitalRead(SWITCH_PIN) == LOW)
+    while (digitalRead(SWITCH_PIN) == LOW) // SUPER simple debounce method
       delay(10);
   }
-  else if (!digitalRead(PLAY_PIN))
-  {
+  else if (!digitalRead(PLAY_PIN)) // if the play/pause button is pressed send the respective
+  {                                 // command to the pc
     Consumer.write(MEDIA_PLAY_PAUSE);   
     while (digitalRead(PLAY_PIN) == LOW)
       delay(10);
   }
-  else if (!digitalRead(NEXT_PIN))
-  {
+  else if (!digitalRead(NEXT_PIN)) // if the next button is pressed send the respective
+  {                                // command to the pc
     Consumer.write(MEDIA_NEXT);   
     while (digitalRead(NEXT_PIN) == LOW)
       delay(10);
   }
-  else if (!digitalRead(PREVIOUS_PIN))
-  {
+  else if (!digitalRead(PREVIOUS_PIN)) // if the pricous button is pressed send the respective
+  {                                    // command to the pc
     Consumer.write(MEDIA_PREVIOUS);   
     while (digitalRead(PREVIOUS_PIN) == LOW)
       delay(10);
@@ -75,9 +92,9 @@ void loop()
   // or decremented
   static int pos,new_pos = 0;
   
-  encoder.tick();
+  encoder.tick(); // checks the encoder position ( part of the encoder library)
   
-  new_pos = encoder.getPosition();
+  new_pos = encoder.getPosition(); // get the new encoder position
   if (new_pos > pos) // if it was increment lets turn up the volume!
   {
     Consumer.write(MEDIA_VOLUME_UP);
